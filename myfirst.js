447904d0591b2123e053725427464fd0697d7555
sahilranadive <32863240+sahilranadive@users.noreply.github.com>
2018-06-22 12:51:03 +0530

    Add files via upload
    Server Creation and Hello World using JS. Run in Node.js.
    Use of response and request in createServer.
    Using the header file to mention content type 
    responding with hello world
    using console.log
    Reading html files into js using the fs module

---
+++ myfirst.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function(req,res){
+  res.writeHead('200',{'Content-Type': 'text/html'});
+//  res.write(req.url);
+  var q = url.parse(req.url,true).query;
+  var txt = q.time+" "+q.month;
+  res.end(txt);
+}).listen(8080);
---
+++ read_html.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req,res){
+  fs.readFile('new_html.html',function(err,data){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8081);
+
+console.log("Hello World");
